02e664c22e1cbf1d78efdbe0b85b5146e26637d3
thelastnicko <thelastnicko@gmail.com>
2022-07-02 19:09:13 -0400

    feature:add icons to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import {AiOutlineHome} from "react-icons/ai"
+import {AiOutlineUser} from "react-icons/ai"
+import {BiBook} from "react-icons/bi"
+import {RiServiceLine} from "react-icons/ri"
+import {BiMessageSquareDetail} from "react-icons/bi"
+
 const Navbar = () => {
-  return <section>Navbar</section>;
+  return <nav>
+    <a href="#home"><AiOutlineHome/></a>
+    <a href="#about"><AiOutlineUser/></a>
+    <a href="#experience"><BiBook/></a>
+    <a href="#services"><RiServiceLine/></a>
+    <a href="#contact"><BiMessageSquareDetail/></a>
+
+  </nav>;
 };
 
 export default Navbar;
