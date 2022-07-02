b17f22b0bebb32e5869fcb7aed59a1fd163c84ab
nicolas <thelastnicko@gmail.com>
2022-07-02 01:57:47 -0400

    featture:add header

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -1,7 +1,17 @@
 import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        My Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Get in touch
+      </a>
+    </div>
+  );
 };
 
 export default CTA;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Header.scss";
+import CTA from "../CTA/CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id="home">
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Nicolas Fernandino</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
