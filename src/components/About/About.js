2b994b96b767afe4bce0878f6b9c41a96178ea56
nicolas <thelastnicko@gmail.com>
2022-07-02 20:45:16 -0400

    feature: add elements to the about asection

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,51 @@
 import React from "react";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 import "./About.scss";
 
+import me from "../../assets/me-about.jpg";
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt="about image"></img>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2 years</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            I’ve spent the last several months in a remote environment, working
+            with HTML, CSS, JavaScript, building everything from landing pages
+            to APIs. My experience as a teacher in an international school
+            helped me collaborate with fellow developers from all over the
+            world. Additionally, working with kids isn’t an easy task, however,
+            it has helped me become efficient, productive, and fast even in a
+            very stressful environment, which is an important skill in the tech
+            industry.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
