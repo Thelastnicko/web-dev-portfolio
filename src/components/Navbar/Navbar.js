28c4b6791b3277de18c96886423eed05a399066c
nicolas <thelastnicko@gmail.com>
2022-07-02 20:10:26 -0400

    feature: manage nav states using useState hook

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,54 @@
 import React from "react";
+import { useState } from "react";
+import "./Navbar.scss";
 
-import {AiOutlineHome} from "react-icons/ai"
-import {AiOutlineUser} from "react-icons/ai"
-import {BiBook} from "react-icons/bi"
-import {RiServiceLine} from "react-icons/ri"
-import {BiMessageSquareDetail} from "react-icons/bi"
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Navbar = () => {
-  return <nav>
-    <a href="#home"><AiOutlineHome/></a>
-    <a href="#about"><AiOutlineUser/></a>
-    <a href="#experience"><BiBook/></a>
-    <a href="#services"><RiServiceLine/></a>
-    <a href="#contact"><BiMessageSquareDetail/></a>
-
-  </nav>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#home"
+        onClick={() => setActiveNav("#home")}
+        className={activeNav === "#home" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <section>Testimonials</section>;
+  return <section id="testimonials">Testimonials</section>;
 };
 
 export default Testimonials;
